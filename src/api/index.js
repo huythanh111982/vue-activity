e30abf1a3955b4f732f379aa60c077c67b6bf232
huythanh111982 <thanhhuy@kiara.local>
2020-11-12 22:56:06 +0700

    ref #10 asynchronous fetching of data Activities

--- src/api/index.js
+++ src/api/index.js
@@ -1,26 +1,31 @@
 import cuid from "cuid";
+const activities = {
+  1546968934: {
+    id: "1546968934",
+    title: "Learn Vue.js",
+    notes: "I started today and it was not good.",
+    progress: 0,
+    category: "1546969049",
+    createdAt: 1546969144391,
+    updatedAt: 1546969144391,
+  },
+  1546969212: {
+    id: "1546969212",
+    title: "Read Witcher Books",
+    notes: "These books are super nice",
+    progress: 0,
+    category: "1546969049",
+    createdAt: 1546969144391,
+    updatedAt: 1546969144391,
+  },
+}
 // Fetch Data Activity
 export const fetchActivities = () => {
-  return {
-    1546968934: {
-      id: "1546968934",
-      title: "Learn Vue.js",
-      notes: "I started today and it was not good.",
-      progress: 0,
-      category: "1546969049",
-      createdAt: 1546969144391,
-      updatedAt: 1546969144391,
-    },
-    1546969212: {
-      id: "1546969212",
-      title: "Read Witcher Books",
-      notes: "These books are super nice",
-      progress: 0,
-      category: "1546969049",
-      createdAt: 1546969144391,
-      updatedAt: 1546969144391,
-    },
-  }
+  return new Promise((resolve,reject) => {
+    setTimeout(() => {
+      resolve(activities)
+    }, 1000);
+  })
 }
 // Fetch Data User
 export const fetchUser = () => {
@@ -44,10 +49,10 @@ export const fetchCatigories = () => {
 // Create New Activity
 export const createActivityAPI = (newActivity) => {
   newActivity.id = cuid();
-  newActivity.progress = 0 ;
+  newActivity.progress = 0;
   newActivity.createdAt = new Date().getTime();
   newActivity.updatedAt = new Date().getTime();
-  return new Promise((resolve)=>{
+  return new Promise((resolve) => {
     return resolve(newActivity);
   })
 }
