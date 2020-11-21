c5f1cda8b0ea4422320e7165d005c79a8d045ab9
huythanh111982 <thanhhuy@kiara.local>
2020-11-21 20:52:28 +0700

    ref #18 Implement Delete

--- src/components/ActivityItem.vue
+++ src/components/ActivityItem.vue
@@ -1,6 +1,11 @@
 <template>
   <article class="post">
-    <h4>{{ activity.title | uppercase }}</h4>
+    <div class="activity-title-wrapper">
+      <h4 class="activity-title">{{ activity.title }}</h4>
+      <i
+        class="fas fa-cog activity-settings"
+        @click="isMenuPlayed =!isMenuPlayed" />
+    </div>
     <p>{{ capitilize(categories[activity.category].text) }}</p>
     <p>{{ activity.notes }}</p>
     <div class="media">
@@ -18,8 +23,16 @@
       </div>
       <div class="media-right">
         <span><span :class="'color-' + activityProgress">{{ activity.progress }} %</span></span>
-        <!-- <span><span :style="{'color':activityProgress}">{{ activity.progress }} %</span></span> -->
+      <!-- <span><span :style="{'color':activityProgress}">{{ activity.progress }} %</span></span> -->
       </div>
+    </div>
+    <div
+      v-if="isMenuPlayed" 
+      class="activity-controll">
+      <a class="button is-warning">Edit</a>
+      <a
+        class="button is-danger"
+        @click="deleteActivity">Delete</a>
     </div>
   </article>
 </template>
@@ -36,37 +49,76 @@ export default {
       required: true,
     },
   },
+  data() {
+    return {
+      isMenuPlayed: false 
+    }
+  },
   computed: {
     activityProgress() {
       const progress = this.activity.progress;
-      if(progress <=0){
+      if (progress <= 0) {
         return 'red'
-      }else if (progress <=50){
+      } else if (progress <= 50) {
         return 'orange'
-      }else{
+      } else {
         return 'green'
       }
     }
   },
   methods: {
     capitilize(word) {
-      if(word && typeof word === 'string'){
-        return word.charAt(0).toUpperCase() + word.slice(1) ;
+      if (word && typeof word === 'string') {
+        return word.charAt(0).toUpperCase() + word.slice(1);
       }
+    },
+    deleteActivity(){
+      this.$emit('activityDeleted', this.activity)
     }
   },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
+
 <style scoped>
-.color-red{
+.color-red {
   color: red;
 }
-.color-orange{
-  color:orange
+
+.color-orange {
+  color: orange
 }
-.color-green{
-  color:green
+
+.color-green {
+  color: green
+}
+
+.activity-title {
+  margin-bottom: 5px;
+  display: inline-block;
+}
+
+.activity-settings {
+  float: right;
+  font-size: 22px;
+
+}
+
+.activity-settings:hover {
+  cursor: pointer;
+}
+
+.activity-controll {
+  margin: 20px 0 0 0;
+
+}
+
+.activity-controll a {
+  margin-right: 5px;
+}
+
+.post .title {
+  margin-bottom: 5px;
 }
 </style>
